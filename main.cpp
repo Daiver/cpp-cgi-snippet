aef2c83ac7dc7e9d7f02933be7bc54ffd10c3736
daiver <ra22341@ya.ru>
2014-10-14 21:25:09 +0400

    check meth

--- main.cpp
+++ main.cpp
@@ -7,5 +7,7 @@ int main()
 {
     printf("Status: 200\nContent-type: text/html\n\n");
     printf("Every thing is OK");
+    char *data = getenv("REQUEST_METHOD");
+    printf("METH TYPE %s", data);
     return 1;
 }
