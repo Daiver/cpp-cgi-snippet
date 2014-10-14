451522c4de454a61a840dc99de914ce329fbcf59
daiver <ra22341@ya.ru>
2014-10-15 00:10:45 +0400

    processing args 2

--- main.cpp
+++ main.cpp
@@ -4,11 +4,17 @@
 #include "requesthandler.h"
 #include "common.h"
 
+
+
 int main()
 {
     printf("Status: 200\nContent-type: text/html\n\n");
     cgi::RequestHandler request;
     cgi::RequestArgs args = request.getArgs();
-    std::cout << readFile("../templates/index.html") << "\n" << args.size();
+    if(args.count("resource")){//process rest
+        std::cout << "{" + args["resource"] + "}";
+    }else{
+        std::cout << readFile("../templates/index.html") << "\n" << args.size();
+    }
     return 1;
 }
