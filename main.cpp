1af0f03a7f26fd4506e5cc705e0f88cbcaa61a42
daiver <ra22341@ya.ru>
2014-10-14 21:11:37 +0400

    main and getpost added

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+
+#include "getpost.h"
+
+int main()
+{
+    printf("Status: 200\nContent-type: text/html\n\n");
+    printf("Every thing is OK");
+    return 1;
+}
