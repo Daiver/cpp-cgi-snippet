daef7fbd7185da7972f0196c5b0eb606e0490e8b
daiver <ra22341@ya.ru>
2014-10-16 22:15:52 +0400

    first bad test

--- tests.cpp
+++ tests.cpp
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <string>
+#include <map>
 
 #include "minitest.h"
+#include "commoncgi.h"
 #include "templateengine.h"
+
+using namespace cgi;
 
 void testTemplateEngine01()
 {
+    RequestArgs args;
+    args["x"] = "10";
+    std::string testTemplate = "123 {{x}} 321";
+    TemplateRenderEither e = TemplateEngine::renderTemplate(testTemplate, args);
+    ASSERT(!e.isLeft);
+    ASSERT_EQ(e.getValue(), std::string("123 10 321"));
 }
 
 int main()
