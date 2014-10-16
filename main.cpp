6ec5bffb3ff6942cce4b6eb6dace69e93e02c90e
daiver <ra22341@ya.ru>
2014-10-16 22:59:36 +0400

    fix json

--- main.cpp
+++ main.cpp
@@ -22,8 +22,7 @@ int main(int argc, char **argv)
     cgi::RequestArgs cont;
     cont["someVar"] = "YEP!";
     if(args.count("resource")){//process rest
-        std::cout << "def";
-        std::cout << "{text: " + args["resource"] + "}";
+        std::cout << "{\"text\": \"" + args["resource"] + "\"}";
     }else{
         std::cout << 
             cgi::TemplateEngine::renderTemplate(readFile(pathToExe + "../templates/index.html"), cont).getValue();
