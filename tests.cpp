3010146eb9c6af5b098460b5c4eb32955e8658ee
daiver <ra22341@ya.ru>
2014-10-23 00:50:58 +0400

    first db test

--- tests.cpp
+++ tests.cpp
@@ -6,6 +6,7 @@
 #include "minitest.h"
 #include "commoncgi.h"
 #include "templateengine.h"
+#include "mysqlworker.h"
 
 using namespace cgi;
 
@@ -49,11 +50,39 @@ void testTemplateEngine04()
     ASSERT_EQ(e.getLeft(), std::string("Bad var name: x"));
 }
 
+void testDB01()
+{
+    MySQLWorker mysql;
+    mysql.connect("192.168.10.101", "root", "123", "pract");
+    ASSERT(mysql.isConnected());
+    MySQLWorker::SQLQueryResult res1 = mysql.query("CREATE DATABASE testDB");
+    MySQLWorker::SQLQueryResult res2 = mysql.query("DROP DATABASE testDB");
+    ASSERT(!res1.isLeft);
+    ASSERT(!res2.isLeft);
+}
+
+void dataBaseTests()
+{
+    bool isPossibleToRunDBTests = false;
+    {
+        MySQLWorker mysql;
+        mysql.connect("192.168.10.101", "root", "123", "pract");
+        isPossibleToRunDBTests = mysql.isConnected();
+    }
+    if(isPossibleToRunDBTests){
+        printf("Yeah, running db tests\n");
+        RUN_TEST(testDB01);
+    }else{
+        printf("Cannot runn db tests\n");
+    }
+}
+
 int main()
 {
     RUN_TEST(testTemplateEngine01);
     RUN_TEST(testTemplateEngine02);
     RUN_TEST(testTemplateEngine03);
     RUN_TEST(testTemplateEngine04);
+    dataBaseTests();
     return 0;
 }
