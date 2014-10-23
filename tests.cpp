a4ac86b6d2a0327195771198e5c92ba1453bb8c2
daiver <ra22341@ya.ru>
2014-10-23 08:43:26 +0400

    new tests

--- tests.cpp
+++ tests.cpp
@@ -68,11 +68,13 @@ void testDB02()
     //MySQLWorker::SQLQueryResult res1 = mysql.query("CREATE DATABASE testDB");
     MySQLWorker::SQLQueryResult res1 = mysql.query("CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), age INT);");
     MySQLWorker::SQLQueryResult res2 = mysql.query("insert into pet values('Bobby', 'Vasya', 13) ");
+    MySQLWorker::SQLQueryResult res4 = mysql.query("insert into pet values('Boo', 'Vasya', 103) ");
     MySQLWorker::SQLQueryResult res3 = mysql.query("select * from pet ");
     //MySQLWorker::SQLQueryResult res2 = mysql.query("DROP DATABASE testDB");
     ASSERT(!res1.isLeft);
     ASSERT(!res2.isLeft);
     ASSERT(!res3.isLeft);
+    ASSERT(!res4.isLeft);
     for(int i = 0; i < res3.getValue().size(); ++i){
         for(int j = 0; j < res3.getValue()[i].size(); ++j){
             printf("%s ", res3.getValue()[i][j].c_str());
@@ -93,6 +95,9 @@ void dataBaseTests()
         printf("Yeah, running db tests\n");
         RUN_TEST(testDB01);
         RUN_TEST(testDB02);
+        MySQLWorker mysql;
+        mysql.connect("192.168.10.101", "root", "123", "pract");
+        mysql.query("DROP DATABASE testDB");
     }else{
         printf("Cannot runn db tests\n");
     }
